41ef59f7f12b6a01ebde7b2a417858a4318769df
FreeballinTradeManager <nataliamoonartist@gmail.com>
2026-03-17 07:50:51 -0400

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# Fix Railway postgres URL automatically
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
